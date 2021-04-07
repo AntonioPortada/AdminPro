571e160a8afc6c0e1c2893f9c2c3e4a8b494e79b
Jorge Antonio Portada Luna <jorge.portada@DIRPRESUPUESTO.LOCAL>
2021-04-07 10:44:20 -0500

    fin sección 3 -diseño

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: [ './login.compoment.css' ]
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
